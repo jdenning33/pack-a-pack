92f82893cf4530821da90cacb1d4b5df9442f774
Jason Denning <jdenning33@gmail.com>
2024-10-20 17:07:12 -0600

    Hide checkboxes for read-only packs

--- src/features/kit/components/card/KitCard.tsx
+++ src/features/kit/components/card/KitCard.tsx
@@ -117,15 +117,17 @@ function KitItemLineItem({
 
     return (
         <li key={item.id} className='flex items-center space-x-2 w-fit'>
-            <Checkbox
-                id={`item-${item.id}`}
-                checked={item.isPacked}
-                onCheckedChange={(_) => toggleItem(item)}
-                onClick={(e) => {
-                    e.stopPropagation();
-                }}
-                disabled={isReadOnly}
-            />
+            {!isReadOnly && (
+                <Checkbox
+                    id={`item-${item.id}`}
+                    checked={item.isPacked}
+                    onCheckedChange={(_) => toggleItem(item)}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                    }}
+                    disabled={isReadOnly}
+                />
+            )}
             <label
                 htmlFor={`item-${item.id}`}
                 onClick={(e) => {
